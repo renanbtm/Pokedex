108598a45ea1b035a7fca6ef719fe6d453911d33
Renan Machado <renanbrunotm21@outlook.com>
2018-02-13 15:48:13 -0200

    Item Detail Dialog added

---
+++ src/api_model/Attributes.java
@@ -0,0 +1,21 @@
+package api_model;
+
+public class Attributes {
+	
+	private String name;
+	private String url;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
---
+++ src/api_model/EffectEntries.java
@@ -0,0 +1,21 @@
+package api_model;
+
+public class EffectEntries {
+	
+	private String short_effect;
+	private String effect;
+	
+	public String getShort_effect() {
+		return short_effect;
+	}
+	public void setShort_effect(String short_effect) {
+		this.short_effect = short_effect;
+	}
+	public String getEffect() {
+		return effect;
+	}
+	public void setEffect(String effect) {
+		this.effect = effect;
+	}
+	
+}
